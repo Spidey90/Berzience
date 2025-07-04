eebb2ea9b8e52b0934ed9f19d0ac971da5d01781
Spidonkey <chitraraj2008@gmail.com>
2025-07-04 14:44:58 +0200

    removed useless files and added hamburger navigation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const hamburger = document.getElementById("hamburger");
 const sidebar = document.getElementById("phone_menu");
 const sidebarList = document.getElementById("navLinks");
 const dropdownMenu = document.getElementById("dropdownMenu");
@@ -5,6 +6,7 @@ const carret = document.getElementById("carret");
 
 function menubar() {
   const isClosed = sidebar.style.width == "0vw";
+  hamburger.classList[isClosed ? "add" : "remove"]("menu-active")
   sidebar.style.width = isClosed ? "72vw" : "0vw";
   sidebarList.style.opacity = isClosed ? "1" : "0";
   sidebarList.style.marginLeft = isClosed ? "32px" : "0";
